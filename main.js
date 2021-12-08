5252d149d43309e184945de3eb7a107c145defd1
Karwer <1972185@johnabbottcollege.net>
2021-12-08 12:52:15 -0500

    add a drop down menu

--- main.js
+++ main.js
@@ -5,19 +5,40 @@ let stationInfo= "http://10.101.0.12:8080/stations/10" //information for a given
 //http://10.101.0.12:8080/distance/Sainte-Dorothée/Bois-Franc //distance in km between first station name (Sainte-Dorothée) to the oher (Bois-Franc)
 //http://10.101.0.12:8080/averageTrainSpeed // the average speed in km/s for all trains in the network
 
-let stationLists = document.getElementById("stationsLists")
-
-function GetStationLists (data)
+async function GetStationLists (data,id)
 {
-    console.log(data)
-}
-Fetch(stationListsURL,GetStationLists) 
 
-async function Fetch( url, doStuff){
+    for (let i = 0; i < data.length; i++){
+        let option = document.createElement("option");
+        option.text = data[i].Name;
+        option.value = data[i].Name;
+        let start_end_Station = document.getElementById(id);
+        start_end_Station.appendChild(option);
+    }
+}
+
+function GetStationInfo (data){
+    
+}
+
+Fetch3(stationListsURL,GetStationLists,"start-station") 
+Fetch3(stationListsURL,GetStationLists,"end-station") 
+
+async function Fetch2( url, doStuff,idElement){
     try {
         response= await fetch( url )
         response= await response.json()
-        response=  doStuff(response)
+        response=  doStuff(response,idElement)
+    } catch (error) {
+        console.log('Error:' , error);
+    }
+};
+
+async function Fetch3( url, doStuff,idElement){
+    try {
+        response= await fetch( url )
+        response= await response.json()
+        response=  doStuff(response,idElement)
     } catch (error) {
         console.log('Error:' , error);
     }
